702bec3b9c503a78e3db3868cb2791ef563c361a
Moustafa Nagaty <moustafro@hotmail.com>
2018-03-13 02:27:59 +0200

    Add update user password, refactored signup user to use stored procedure, checked for duplicate email address in sign up user

---
+++ src/main/java/commands/UpdateUser.java
@@ -0,0 +1,48 @@
+package commands;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Envelope;
+import commands.Command;
+import model.User;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class UpdateUser extends Command {
+
+    public void execute() {
+        HashMap<String, Object> props = parameters;
+
+        Channel channel = (Channel) props.get("channel");
+        JSONParser parser = new JSONParser();
+        int id = 0;
+        String password = "";
+        try {
+            //Check body, what values should be updated? password? username? etc. Call the correct corresponding function.
+            JSONObject body = (JSONObject) parser.parse((String) props.get("body"));
+//            System.out.println("The BODY is: " + body.toString());
+            JSONObject params = (JSONObject) parser.parse(body.get("body").toString());
+            id = Integer.parseInt(params.get("id").toString());
+            password = params.get("password").toString();
+            System.out.println("UserId: " + id + ", password: " + password);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        AMQP.BasicProperties properties = (AMQP.BasicProperties) props.get("properties");
+        AMQP.BasicProperties replyProps = (AMQP.BasicProperties) props.get("replyProps");
+        Envelope envelope = (Envelope) props.get("envelope");
+        String response = User.changePasswordById(id,password);
+//        String response = (String)props.get("body");
+        try {
+            channel.basicPublish("", properties.getReplyTo(), replyProps, response.getBytes("UTF-8"));
+            channel.basicAck(envelope.getDeliveryTag(), false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
